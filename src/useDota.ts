9eca6ae9b70c61c4cebdb72437a48b779291749d
rayxiong <rayxiong@didiglobal.com>
2025-01-01 18:12:18 +0800

    去掉Hero的export导出，版本升到1.0.5

--- src/useDota.ts
+++ src/useDota.ts
@@ -3,7 +3,7 @@ import { onMounted, ref } from 'vue'
 
 // Dota2的openAPI文档：https://docs.opendota.com/#tag/heroes/operation/get_heroes
 
-export interface Hero {
+interface Hero {
   id: number
   name: string
   localized_name: string
